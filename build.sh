895a9a9497a5e85827100dbdef971127c93e0e29
Alex O Regan <alexsoregan@gmail.com>
2020-12-30 19:09:28 +0000

    uselessly vague glob removal

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-gcc -I. $(pkg-config --cflags dbus-1) main.c ./mpris/* ./util/** \
+gcc -I. $(pkg-config --cflags dbus-1) main.c ./mpris/* ./util/* \
     $(pkg-config --libs dbus-1) \
     -g -O0\
     -o playplause
