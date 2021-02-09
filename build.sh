2bacb678a7172b3a8a59a180c6d1dad8d6f26079
Alex O Regan <alexsoregan@gmail.com>
2021-02-09 22:29:16 +0000

    generic build tuning for x86

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-gcc -I. $(pkg-config --cflags dbus-1) main.c ./mpris/* ./util/* \
+
+# if you really want to squeeze your performance, use -mtune=native and build from source :D
+gcc -mtune=generic -march=x86-64\
+    -I. $(pkg-config --cflags dbus-1) main.c ./mpris/* ./util/* \
     $(pkg-config --libs dbus-1) \
     -g -O0\
     -o mediactl
